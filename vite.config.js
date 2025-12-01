b0d4551b978985cedd49ccdab54e09dcdc6ef8a5
renvis182 <renvis182@hotmail.com>
2025-12-01 11:44:11 -0500

    Actualizacion de configuracion y dependencias

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/renvis182/react",
+  base: "/react",
   plugins: [react()],
 })
